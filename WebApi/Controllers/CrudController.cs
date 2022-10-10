807654f291040bc72f9a66ed3f229e486ac41322
Gilberto Pavanelli <gilberto.pavanelli.jr@gmail.com>
2022-10-10 03:04:41 -0300

    Add project files.

---
+++ WebApi/Controllers/CrudController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    public class CrudController : Controller
+    {
+        CrudDbContext _context;
+        public CrudController(CrudDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        [Route("/api/cruds")]
+        public async Task<IActionResult> GetCruds()
+        {
+            var cruds = await _context.Cruds.ToListAsync();
+            return Ok(cruds);
+        }
+        [HttpGet]
+        [Route("/api/cruds/{id}")]
+        public async Task<IActionResult> GetCrud(Guid id)
+        {
+            var crud = await _context.Cruds.SingleOrDefaultAsync(x => x.Id == id);
+            if (crud == null)
+                return NotFound();
+            else
+                return Ok(crud);
+        }
+        [HttpPost]
+        [Route("/api/cruds")]
+        public async Task<IActionResult> AddCrud([FromBody] Crud crud)
+        {
+            crud.Id = Guid.NewGuid();
+            await _context.Cruds.AddAsync(crud);
+            await _context.SaveChangesAsync();
+            return Ok(crud);
+        }
+        [HttpPut]
+        [Route("/api/cruds/{id}")]
+        public async Task<IActionResult> UpdateCrud([FromRoute] Guid id, [FromBody] Crud newCrud)
+        {
+            var crud = await _context.Cruds.SingleOrDefaultAsync(x => x.Id == id);
+
+            if (crud == null)
+                return NotFound();
+            crud.Name = newCrud.Name;
+            crud.Phone = newCrud.Phone;
+            crud.Department = newCrud.Department;
+            crud.Salary = newCrud.Salary;
+            crud.Email = newCrud.Email;
+            await _context.SaveChangesAsync();
+            return Ok(crud);
+        }
+        [HttpDelete]
+        [Route("/api/cruds/{id}")]
+        public async Task<IActionResult> DeleteCrud([FromRoute] Guid id)
+        {
+            var crud = await _context.Cruds.FindAsync(id);
+            if (crud == null)
+                return NotFound();
+            _context.Cruds.Remove(crud);
+            await _context.SaveChangesAsync();
+            return Ok(crud);
+        }
+    }
+}
---
+++ WebApi/Models/CrudDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Models
+{
+    public class CrudDbContext : DbContext
+    {
+        public CrudDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Crud> Cruds { get; set; }
+    }
+}
